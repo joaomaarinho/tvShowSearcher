583cd3bb35838c8a27e1ef9ea7e8f8c271ea032a
João Souza <56441159+joaomaarinho@users.noreply.github.com>
2021-09-07 11:56:34 -0300

    Add files via upload

---
+++ TV SHOW API/app.js
@@ -0,0 +1,30 @@
+const resetButton = document.querySelector('#reset');
+
+const form = document.querySelector('#searchForm');
+form.addEventListener('submit', async function(e) {
+    e.preventDefault();
+    const searchTerm = form.elements.query.value;
+    const config = { params: { q: searchTerm } };
+    const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+    makeImages(res.data);
+    form.elements.query.value = '';
+})
+
+const makeImages = (shows) => {
+    for(let result of shows){
+        if (result.show.image) {
+            const img = document.createElement('IMG');
+            img.src = result.show.image.medium;
+            document.body.append(img);
+        }        
+    }
+}
+
+const resetImgs = function () {
+    const imgs = document.querySelectorAll('img');
+    for(let img of imgs){
+        img.remove();
+    }
+}
+
+resetButton.addEventListener('click', resetImgs);
